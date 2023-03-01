d9b4099b72a3b08ae6f397f65953892c00ba6493
GioGio4 <g.ceccobelli@outlook.it>
2023-03-01 15:27:10 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<template>
+  <h1 class="my-5">Benvenuto</h1>
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.mount("#app");
